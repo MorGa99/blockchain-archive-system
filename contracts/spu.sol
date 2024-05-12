a6d796f18ac387a41b67f148ae2d8cc004c4c0c9
MorGa99 <jalalanofal@gmail.com>
2024-05-12 07:29:45 -0900

    Empty-Commit

--- contracts/spu.sol
+++ contracts/spu.sol
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
-
 contract spu {
     address is_admin;
     constructor() {
         is_admin = msg.sender;
     }
 
-    //---------------------------------- structures ---------------------------------------------------------------
     struct course_grade {
         uint student_id;
         uint mark;
@@ -24,6 +22,7 @@ contract spu {
         string name;
         string weekly_hour;
         string level;
+        string college;
         course_grade[] students_grades;
     }
 
@@ -59,8 +58,6 @@ contract spu {
         student_courses[] all_courses;
     }
 
-    //---------------------------------- tools ----------------------------------------------------------------------
-
     modifier onlyemp(address v) {
         require(ChekIfEmp(v) == true);
         _;
@@ -88,27 +85,30 @@ contract spu {
 
     address[] private all_profs;
     uint[] private all_profs_id;
-    //------------------------------------- Auxiliary functions --------------------------------------
-    function chekRole(address a) private view returns (uint) {
+
+    address[] public authorized;
+
+    function checkRole(address a) public view returns (uint) {
         if (a == is_admin) {
             return 1;
         } else if (ChekIfEmp(msg.sender) == true) {
             return 2;
-        } else if (ChekIfstudentaddress(msg.sender) == true) {
-            return 3;
-        } else if (ChekIfprofaddress(msg.sender) == true) {
+        } else if (
+            ChekIfauthorizedaddress(msg.sender) == true ||
+            ChekIfprofaddress(msg.sender) == true
+        ) {
             return 3;
         } else {
             return 4;
         }
     }
     function ChekIfEmp(address y) public view returns (bool b) {
-        uint i = 0;
-        while (i < e_counter) {
-            i++;
-            if (y == all_employees[i - 1]) return true;
-            else return false;
+        for (uint i = 0; i < all_employees.length; i++) {
+            if (y == all_employees[i]) {
+                return true;
+            }
         }
+        return false;
     }
     function ChekIfstudent(uint _id) private view returns (bool b) {
         for (uint i = 0; i < all_students_id.length; i++) {
@@ -122,6 +122,14 @@ contract spu {
     function ChekIfstudentaddress(address x) private view returns (bool b) {
         for (uint i = 0; i < all_students.length; i++) {
             if (x == all_students[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+    function ChekIfauthorizedaddress(address x) private view returns (bool b) {
+        for (uint i = 0; i < authorized.length; i++) {
+            if (x == authorized[i]) {
                 return true;
             }
         }
@@ -176,99 +184,42 @@ contract spu {
     function add_address_to_student(
         uint _id,
         address _address
-    ) public onlyemp(msg.sender) returns (string memory) {
-        string memory err = "id is not correct";
-        string memory don = "entered successfully";
-
+    ) public onlyemp(msg.sender) {
         if (ChekIfstudent(_id) == true) {
             student storage s = get_student[_id];
             s.student_address = _address;
             all_students.push(_address);
-            return don;
-        } else return err;
+        }
     }
 
     function add_address_to_prof(
         uint _id,
         address _address
-    ) public onlyemp(msg.sender) returns (string memory) {
-        string memory err = "id is not correct";
-        string memory don = "entered successfully";
-
+    ) public onlyemp(msg.sender) {
         if (ChekIfprof(_id) == true) {
             prof storage s = get_prof[_id];
             s.prof_address = _address;
             all_profs.push(_address);
-            return don;
-        } else return err;
+        }
     }
 
-    function validateEmail(string memory _email) internal pure returns (bool) {
-        bytes memory b = bytes(_email);
-        uint emailLength = b.length;
-        bool noAtSign = true;
-        bool noDot = true;
-
-        for (uint i = 0; i < emailLength; i++) {
-            if (b[i] == "@") {
-                noAtSign = false;
-            }
-            if (b[i] == ".") {
-                noDot = false;
-            }
-        }
-
-        return !noAtSign && !noDot;
-    }
-    function isValidDateFormat(
-        string memory _date
-    ) internal pure returns (bool) {
-        bytes memory b = bytes(_date);
-        if (b.length != 10) {
-            return false;
-        }
-
-        if (b[4] != "/" || b[7] != "/") {
-            return false;
-        }
-
-        for (uint i = 0; i < b.length; i++) {
-            if (i == 4 || i == 7) {
-                continue;
-            }
-            if (b[i] < "0" || b[i] > "9") {
-                return false;
-            }
-        }
-
-        return true;
-    }
-    //------------------------------------------ main functions ----------------------------------------------------
     function view_student(uint id) public view returns (student memory k) {
-        if (msg.sender == is_admin) {
-            return get_student[id];
-        } else if (ChekIfEmp(msg.sender) == true) {
-            return get_student[id];
-        } else if (ChekIfstudentaddress(msg.sender) == true) {
-            return get_student[id];
-        } else if (ChekIfprofaddress(msg.sender) == true) {
+        if (
+            (msg.sender) == is_admin ||
+            ChekIfEmp(msg.sender) == true ||
+            (ChekIfstudentaddress(msg.sender) == true &&
+                get_student[id].student_address == (msg.sender)) ||
+            ChekIfprofaddress(msg.sender) == true ||
+            ChekIfauthorizedaddress(msg.sender) == true
+        ) {
             return get_student[id];
         } else {
-            student storage s = get_student[id];
-            student memory v;
-            v.firstName = s.firstName;
-            v.lastName = s.lastName;
-            v.fatherName = s.fatherName;
-            v.motherName = s.motherName;
-            v.gender = s.gender;
+            student memory v = get_student[id];
             v.email = "null";
-            v.collegeName = s.collegeName;
-            v.dateOfBirth = s.dateOfBirth;
             v.mobileNumber = "null";
-            v.cumulative_averge = s.cumulative_averge;
             v.student_address = 0x0000000000000000000000000000000000000000;
-            v.warnnings = s.warnnings;
-            v.all_courses = s.all_courses;
+            v.warnnings = new string[](0);
+            v.all_courses = new student_courses[](0);
             return v;
         }
     }
@@ -277,13 +228,15 @@ contract spu {
         uint id,
         string memory name,
         string memory weekly_hour,
-        string memory level
+        string memory level,
+        string memory college
     ) public onlyAdmin(msg.sender) {
         course storage b = get_course[id];
         b.id = id;
         b.name = name;
         b.weekly_hour = weekly_hour;
         b.level = level;
+        b.college = college;
         spu_courses.push(b);
     }
 
@@ -315,20 +268,6 @@ contract spu {
         string memory _dateOfBirth,
         string memory _mobileNumber
     ) public onlyAdmin(msg.sender) {
-        require(_id > 0);
-        require(bytes(_firstName).length > 0);
-        require(bytes(_lastName).length > 0);
-        require(bytes(_fatherName).length > 0);
-        require(bytes(_motherName).length > 0);
-        require(bytes(_gender).length > 0);
-        require(bytes(_email).length > 0);
-        require(validateEmail(_email));
-        require(bytes(_collegeName).length > 0);
-        require(bytes(_dateOfBirth).length > 0);
-        require(isValidDateFormat(_dateOfBirth));
-        require(bytes(_mobileNumber).length > 0);
-        require(get_student[_id].id == 0);
-
         student storage s = get_student[_id];
         s.id = _id;
         s.firstName = _firstName;
@@ -358,21 +297,6 @@ contract spu {
         string memory _mobileNumber,
         string memory _specialization
     ) public onlyAdmin(msg.sender) {
-        require(_id > 0);
-        require(bytes(_firstName).length > 0);
-        require(bytes(_lastName).length > 0);
-        require(bytes(_fatherName).length > 0);
-        require(bytes(_motherName).length > 0);
-        require(bytes(_gender).length > 0);
-        require(bytes(_email).length > 0);
-        require(validateEmail(_email));
-        require(bytes(_collegeName).length > 0);
-        require(bytes(_dateOfBirth).length > 0);
-        require(isValidDateFormat(_dateOfBirth));
-        require(bytes(_mobileNumber).length > 0);
-        require(bytes(_specialization).length > 0);
-        require(get_prof[_id].id == 0);
-
         prof storage s = get_prof[_id];
 
         s.id = _id;
@@ -402,69 +326,37 @@ contract spu {
         uint _id,
         string memory _warnning
     ) public onlyemp(msg.sender) {
-        string memory w = _warnning;
-        get_student[_id].warnnings.push(w);
-    }
-
-    function get_Courses_Of_Student(
-        uint _id
-    ) public view returns (student_courses[] memory) {
-        return get_student[_id].all_courses;
-    }
-
-    function get_warnnings(uint _id) public view returns (string[] memory b) {
-        string[] memory e;
-        e = get_student[_id].warnnings;
-        return e;
+        get_student[_id].warnnings.push(_warnning);
     }
 
     function Delet_emp(uint n) public onlyAdmin(msg.sender) {
         delete all_employees[n];
         delete get_emp[n];
 
-        for (uint i = n; i < all_employees.length; i++) {
-            all_employees[n] = all_employees[all_employees.length - 1];
-        }
         e_counter--;
     }
 
-    function deleteStudent(uint _id) public onlyAdmin(msg.sender) {
-        require(ChekIfstudent(_id));
-
+    function deleteStudent(uint256 _id) public onlyAdmin(msg.sender) {
         delete get_student[_id];
-        uint indexToDelete;
-        for (uint i = 0; i < all_students_id.length; i++) {
-            if (all_students_id[i] == _id) {
-                indexToDelete = i;
-                break;
-            }
-        }
 
-        if (indexToDelete < all_students_id.length) {
-            all_students_id[indexToDelete] = all_students_id[
-                all_students_id.length - 1
-            ];
-            all_students_id.pop();
+        uint256 lastIndex = all_students_id.length - 1;
+        if (_id != all_students_id[lastIndex]) {
+            all_students_id[_id] = all_students_id[lastIndex];
         }
+        all_students_id.pop();
+
         s_counter--;
     }
 
-    function deleteProfessor(uint _id) public onlyAdmin(msg.sender) {
-        require(ChekIfprof(_id));
-
+    function deleteprof(uint256 _id) public onlyAdmin(msg.sender) {
         delete get_prof[_id];
-        uint indexToDelete;
-        for (uint i = 0; i < all_profs_id.length; i++) {
-            if (all_profs_id[i] == _id) {
-                indexToDelete = i;
-                break;
-            }
-        }
 
-        if (indexToDelete < all_profs_id.length) {
-            all_profs_id[indexToDelete] = all_profs_id[all_profs_id.length - 1];
-            all_profs_id.pop();
+        uint256 lastIndex = all_profs_id.length - 1;
+        if (_id != all_profs_id[lastIndex]) {
+            all_profs_id[_id] = all_profs_id[lastIndex];
         }
+        all_profs_id.pop();
+
         p_counter--;
     }
 
